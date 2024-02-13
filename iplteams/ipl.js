665b92d53729b5809b2e3e75740c67aa324c7d44
Madhavi kunchala <140079534+Madhavikunchala3@users.noreply.github.com>
2024-02-13 18:35:54 +0530

    Add files via upload

---
+++ iplteams/ipl.js
@@ -0,0 +1,16 @@
+let imageElement=document.getElementById("changeteam");
+function csk(){
+    imageElement.src="https://img.jagranjosh.com/images/2022/March/2632022/CSK_player_list_2022_stats_past_records.jpg"
+}
+function rcb(){
+    imageElement.src="https://static.toiimg.com/photo/msid-99528586/99528586.jpg"
+}
+function srh(){
+    imageElement.src="https://bsmedia.business-standard.com/_media/bs/img/article/2023-04/07/full/1680867195-9995.jpg?im=FeatureCrop,size=(826,465)"
+}
+function gt(){
+    imageElement.src="https://www.hindustantimes.com/ht-img/img/2023/04/13/550x309/ANI-20230413339-0_1681413972151_1681413972151_1681413992963_1681413992963.jpg"
+}
+function mi(){
+    imageElement.src="https://images.hindustantimes.com/rf/image_size_630x354/HT/p2/2020/03/09/Pictures/final-of-ipl-t20-2019-csk-vs_7060b2ae-61da-11ea-926b-bd7a11f61927.jpg"
+}
